ea90821662535000b06131351a7a811cf4f542ad
Loop312 <139386975+Loop312@users.noreply.github.com>
2024-07-28 00:52:41 -0400

    Change human exp multiplier mechanic
    Initially their exp gain was double. Now their multiplier starts at x1 and gets stronger with every level up

--- src/characters/Human.kt
+++ src/characters/Human.kt
@@ -6,7 +6,18 @@ Weaknesses: None
 Unique Skill: Exp Multiplier
 */
 class Human : Character() {
+
+    var expMultiplier = 1.0
+    
     override fun gainExp(num: Int){
-        exp += (num*2)
+        exp += (num*expMultiplier)
+    }
+
+    //every level up their multiplier gets stronger
+    override fun lvlup(){
+        exp = exp - explimit
+        explimit *= 2
+        skillPoints += 5
+        expMultiplier += .05
     }
 }
